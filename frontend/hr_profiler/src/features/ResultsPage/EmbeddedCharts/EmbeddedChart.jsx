150530f91adc79e762d155def944ea741f8e02cb
sedhha <activity.schoolsh2@gmail.com>
2021-12-12 19:27:17 +0530

    Updated Embedded Pages

---
+++ frontend/hr_profiler/src/features/ResultsPage/EmbeddedCharts/EmbeddedChart.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function EmbeddedChart() {
+  return (
+    <div>
+      <iframe
+        title='drilldown_chart'
+        width='100%'
+        height='365rem'
+        src='https://datastudio.google.com/embed/reporting/0652cfef-6c51-4441-887d-2a7dd09075af/page/52ZhC'
+        frameBorder='0'
+        style={{ border: 0 }}
+        allowFullScreen></iframe>
+      <br />
+    </div>
+  );
+}
